e9ac8ba180facc354954e4d5e7e3c735b2d130d3
Jordi Lazo <jordilazo94@gmail.com>
2020-12-01 11:05:31 +0100

    JAVA

---
+++ JUnit/Test/calculadoraJavaTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class calculadoraJavaTest {
+
+    @Test
+    void sum() {
+        calculadoraJava cal = new calculadoraJava();
+        int a = 10;
+        int b = 20;
+
+        int result = 30;
+
+        assertEquals(result, cal.sum(a,b));
+
+    }
+}
